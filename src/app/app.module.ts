b6e9dcbf3510445aa04d852428b142644268d498
Morteza Aghili <morteza.aghili90@gmail.com>
2019-08-12 01:00:36 +0430

    Added bootstrap, added search and user list table, added mixin for change width of the table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// Material components!
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSortModule } from "@angular/material/sort";
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { appRoutes } from "./routes";
 import { UsersModule } from './users/users.module';
 
+
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { useHash: true }),
-    UsersModule
+    UsersModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSortModule
   ],
   declarations: [
     AppComponent,
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from "rxjs/internal/Subscription";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 import { UsersRepositoryService } from './services/users.repository.service';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -11,13 +15,36 @@ import { UsersRepositoryService } from './services/users.repository.service';
 export class UsersComponent implements OnInit {
   private subscription: Subscription;
 
+  displayedColumns: string[] = [
+    "photo",
+    "id",
+    "email",
+    "first_name",
+    "phone",
+    "address",
+    "modified",
+    "view"
+  ];
+  dataSource: MatTableDataSource<User>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   constructor(private usersRepoService: UsersRepositoryService) {}
 
   ngOnInit() {
     this.subscription = this.usersRepoService.getUsers()
-    .subscribe(users => {
-      console.table(users);
-    });
+      .subscribe(users => {
+        this.dataSource = new MatTableDataSource(users);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   ngOnDestroy(): void {
